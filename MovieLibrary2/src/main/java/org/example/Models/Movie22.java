6fec84fc789532bece3c93b032b5bfd4e2d0f997
Katarzyna.Wyrzykiewi <katarzyna.wyrzykiewicz@asseco.pl>
2024-03-01 14:56:26 +0100

    add Movie class

---
+++ MovieLibrary2/src/main/java/org/example/Models/Movie22.java
@@ -0,0 +1,18 @@
+package org.example.Models;
+
+public class Movie22 {
+    package org.example.Models;
+
+    public class Actor {
+        private String firstname;
+
+        public String getFirstname() {
+            return firstname;
+        }
+
+        public void setFirstname(String firstname) {
+            this.firstname = firstname;
+        }
+    }
+
+}
