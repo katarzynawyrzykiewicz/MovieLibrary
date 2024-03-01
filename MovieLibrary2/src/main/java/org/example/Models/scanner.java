259ca25c92be8441604de0d14bf9e0fdd46f8c9f
Katarzyna.Wyrzykiewi <katarzyna.wyrzykiewicz@asseco.pl>
2024-03-01 15:11:57 +0100

    add scanner class

---
+++ MovieLibrary2/src/main/java/org/example/Models/scanner.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Witamy na imprezie integracyjnej! jak masz na imię? \n");
+        Scanner scanner = new Scanner(System.in);
+        String name;
+        name = scanner.nextLine();
+
+
+        if (name.equals("Donald"))
+        {
+            System.out.println("Jesteś rudy " + name + "\n" + "Nie masz zaproszenia");
+        }
+        else
+        {
+            System.out.println("Witam " + name + "\n" + "Życzymy miłej zabawy");
+        }
+
+    }
+}
