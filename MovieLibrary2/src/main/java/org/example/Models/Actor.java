fd07f88bbe87dc6085f44b6974134b2f01f91109
Katarzyna.Wyrzykiewi <katarzyna.wyrzykiewicz@asseco.pl>
2024-03-01 14:42:10 +0100

    add Actor class

---
+++ MovieLibrary2/src/main/java/org/example/Models/Actor.java
@@ -0,0 +1,13 @@
+package org.example.Models;
+
+public class Actor {
+    private String firstname;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+}
